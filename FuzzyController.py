160c286f35f25f6dd0205556c2745fd873ce44a4
nickchilders <nickchilders@umail.ucsb.edu>
2008-05-29 19:02:16 +0000

    update

--- FuzzyController.py
+++ FuzzyController.py
@@ -55,17 +55,19 @@ class FuzzyController:
 		self.mid_x = float(size_x)/2.0
 		self.mid_y = float(size_y)/2.0
 
-		self.center = FuzzyTriangle(-0.1,0.1)
+		self.center = FuzzyTriangle(-0.2,0.2)
 
-		self.slight_neg = FuzzyTriangle(-0.3,0.0)
-		self.neg = FuzzyTriangle(-0.6,-0.15)
-		self.far_neg = FuzzyLeftShoulder(-0.75,-0.4)
+		self.slight_neg = FuzzyTriangle(-0.4,0.1)
+		self.neg = FuzzyTriangle(-0.6,-0.2)
+		self.far_neg = FuzzyLeftShoulder(-0.9,-0.3)
 		
-		self.slight_pos = FuzzyTriangle(0.0,0.3)
-		self.pos = FuzzyTriangle(0.15,0.6)
-		self.far_pos = FuzzyRightShoulder(0.4,0.75)
+		self.slight_pos = FuzzyTriangle(0.1,0.4)
+		self.pos = FuzzyTriangle(0.2,0.6)
+		self.far_pos = FuzzyRightShoulder(0.3,0.9)
 
-	def update(self, x, y, valid):
+		self.tc = TurretControl()
+
+	def update(self, x, y):
 		x_val = (float(x) - self.mid_x)/self.mid_x
 		y_val = (float(y) - self.mid_y)/self.mid_y
 		
@@ -75,8 +77,15 @@ class FuzzyController:
 		print "X RESULT:",x_result
 		print "Y RESULT:",y_result
 
+		if(not (-0.05 < x_result and x_result < 0.05)):
+			if(x_result < 0.0):
+				self.tc.left(-45*x_result)
+			else:
+				self.tc.right(45*x_result)
+
 	def __calcResult(self,val):
 
+		"""
 		print "fr:",self.far_pos.membership(val)
 		print "r:",self.pos.membership(val)
 		print "sr:",self.slight_pos.membership(val)
@@ -84,7 +93,7 @@ class FuzzyController:
 		print "sl:",self.slight_neg.membership(val)
 		print "l:",self.neg.membership(val)
 		print "fl:",self.far_neg.membership(val)
-		
+		"""
 		cen = self.center.membership(val)
 
 		sn = self.slight_neg.membership(val)
@@ -95,12 +104,12 @@ class FuzzyController:
 		p = self.pos.membership(val)
 		fp = self.far_pos.membership(val)
 	
-		neg_value = -1*fn*fn + -0.5*n*n + -0.2*sn*sn	
+		neg_value = -1*fn*fn + -0.5*n*n + -0.1*sn*sn	
 		pos_value = 1*fp*fp + 0.5*p*p + 0.2*sp*sp	
 		result = (neg_value + pos_value)/(fn*fn + n*n + sn*sn + cen*cen + sp*sp + p*p + fp*fp)
 		
 		return result
 
 
-bob = FuzzyController(240,320)
-bob.update(int(sys.argv[1]),int(sys.argv[2]),False)
+#bob = FuzzyController(240,320)
+#bob.update(int(sys.argv[1]),int(sys.argv[2]),False)
